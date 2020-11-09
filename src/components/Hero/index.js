a7aef6f3398d3744be1830db946b557e7f8b3db9
mccamatt92 <matthias.mccarthy@gmail.com>
2020-11-09 20:00:00 +0100

    Created Functioning Sidebar

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,14 +1,22 @@
 import React, { useState } from 'react';
 import Navbar from '../NavBar';
+import Sidebar from '../Sidebar';
 import {
     HeroBtn,
   HeroContainer, HeroContent, HeroH1, HeroItems, HeroP
 } from './HeroElements';
 
 const Hero = () => {
+    const[isOpen,setIsOpen] =useState(false);
+
+     const toggle= ()=>{
+         setIsOpen(!isOpen);
+     }   
+
     return (
         <HeroContainer>
-            <Navbar/>
+            <Navbar toggle={toggle}/>
+            <Sidebar isOpen={isOpen} toggle={toggle}/>
             <HeroContent>
                 <HeroItems>
                     <HeroH1>Greatest Pizza Ever</HeroH1>
---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import {
+  SidebarContainer,
+  Icon,
+  CloseIcon,
+  SidebarMenu,
+  SidebarLink,
+  SidebarRoute,
+  SideBtnWrap
+} from './SidebarElements';
+
+const Sidebar = ({isOpen,toggle}) => {
+    return (
+        <SidebarContainer isOpen={isOpen} onClick={toggle}>
+            <Icon>
+                <CloseIcon/>
+            </Icon>
+            <SidebarMenu>
+                <SidebarLink to="/">Pizzas</SidebarLink>
+                <SidebarLink to="/">Desserts</SidebarLink>
+                <SidebarLink to="/">FullMenu</SidebarLink>
+                <SidebarLink to="/"></SidebarLink>
+            </SidebarMenu>
+            <SideBtnWrap>
+                <SidebarRoute to="/">Order Now</SidebarRoute>
+            </SideBtnWrap>
+        </SidebarContainer>
+    )
+}
+
+export default Sidebar
